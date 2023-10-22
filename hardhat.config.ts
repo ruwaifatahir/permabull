ec6c454098a8d9e40acb1645560e21fcef15ff2f
ruwaifatahir <66880814+ruwaifatahir@users.noreply.github.com>
2023-10-22 15:12:43 +0500

    init

---
+++ hardhat.config.ts
@@ -0,0 +1,29 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+
+const config: HardhatUserConfig = {
+  solidity: {
+    version: "0.8.19",
+  },
+  networks: {
+    bsc: {
+      url: process.env.BSC_URL,
+      chainId: 56,
+      accounts: { mnemonic: process.env.MNEMONIC },
+    },
+
+    bsctestnet: {
+      url: process.env.BSC_TESTNET_URL,
+      chainId: 97,
+      accounts: { mnemonic: process.env.MNEMONIC },
+    },
+  },
+  etherscan: {
+    apiKey: {
+      bsc: process.env.BSC_API_KEY!,
+      bscTestnet: process.env.BSC_TESTNET_API_KEY!,
+    },
+  },
+};
+
+export default config;
