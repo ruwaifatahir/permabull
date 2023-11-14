5d1d1dbcfa2c57a28c9482253ce710550ac08122
ruwaifatahir <66880814+ruwaifatahir@users.noreply.github.com>
2023-11-14 15:42:37 +0500

    update hardhat config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -4,13 +4,6 @@ import "@nomicfoundation/hardhat-toolbox";
 const config: HardhatUserConfig = {
   solidity: {
     version: "0.6.12", // any version you want
-    settings: {
-      // viaIR: true,
-      optimizer: {
-        enabled: true,
-       
-      },
-    },
   },
 };
 
